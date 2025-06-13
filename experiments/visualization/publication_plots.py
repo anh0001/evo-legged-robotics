13fd8ca07136ddb3ae9ddbfcd51591739215965e
Anhar Risnumawan <anhrisn@gmail.com>
2025-06-13 15:21:56 +0900

    Handle missing keys in parameter sensitivity plot

--- experiments/visualization/publication_plots.py
+++ experiments/visualization/publication_plots.py
@@ -327,15 +327,22 @@ class PublicationVisualizationPipeline:
         for idx, (param1, param2) in enumerate(param_pairs):
             ax = fig.add_subplot(gs[0, idx])
             
-            # Get sensitivity data
-            if response_var in sensitivity_results:
-                data = sensitivity_results[response_var]
+            # Get sensitivity data from nested response_surfaces key
+            if response_var in sensitivity_results['response_surfaces']:
+                data = sensitivity_results['response_surfaces'][response_var]
                 
                 # Create scatter plot with color-coded response
                 if 'samples' in data:
-                    x_vals = [sample[param1] for sample in data['samples']]
-                    y_vals = [sample[param2] for sample in data['samples']]
-                    z_vals = [sample[response_var] for sample in data['samples']]
+                    filtered = [
+                        sample for sample in data['samples']
+                        if param1 in sample and param2 in sample and response_var in sample
+                    ]
+                    if not filtered:
+                        continue
+
+                    x_vals = [sample[param1] for sample in filtered]
+                    y_vals = [sample[param2] for sample in filtered]
+                    z_vals = [sample[response_var] for sample in filtered]
                     
                     scatter = ax.scatter(x_vals, y_vals, c=z_vals, cmap='viridis', alpha=0.7)
                     ax.set_xlabel(param1.replace('_', ' ').title())
